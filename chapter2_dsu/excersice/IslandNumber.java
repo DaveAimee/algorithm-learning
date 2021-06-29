371bfd8b093dc1190c7e7bee490bc3e0aa56aa73
YangLiu <LotusRe@outlook.com>
2021-06-29 16:17:27 +0800

    岛屿数目的unionset解法

--- chapter2_dsu/excersice/IslandNumber.java
+++ chapter2_dsu/excersice/IslandNumber.java
@@ -1,16 +1,80 @@
 package chapter2_dsu.excersice;
 
-import chapter2_dsu.concept.Dsu;
-
+// 题目链接：https://leetcode-cn.com/problems/number-of-islands/
 public class IslandNumber {
+    private int[] moveX = {-1, 1, 0, 0};
+    private int[] moveY = {0, 0, -1, 1};
+
+    class Union {
+        private int[] union;
+        private int count = 0;
+        private boolean isPathCompressEnabled = true;
+
+        Union(char[][] grid) {
+            union = new int[grid.length * grid[0].length];
+            for (int i = 0; i < grid.length; i++) {
+                for (int j = 0; j < grid[0].length; j++) {
+                    if (grid[i][j] == '1') {
+                        int index = i * grid[0].length + j;
+                        // 初始化指向自己
+                        union[index] = index;
+                        this.count++;
+                    }
+                }
+            }
+        }
+
+        public int find(int x) {
+            if (this.union[x] != x) {
+                if (isPathCompressEnabled) {
+                    union[x] = find(union[x]);
+                }
+                return union[x];
+            } else {
+                return x;
+            }
+        }
+
+        public void union(int x, int y) {
+            // 若两个元素的根元素相同，则两个元素在一个
+            if (find(x) == find(y)) {
+                return;
+            }
+            this.union[find(x)] = find(y);
+            this.count --;
+        }
+
+        public int getSetNums() {
+            return this.count;
+        }
+    }
     public int numIslands(char[][] grid) {
-        Dsu d = new Dsu();
-        int nums = grid.length * grid[0].length;
-        d.makeset(nums);
-        
-        return -1;
+        Union u = new Union(grid);
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0;j < grid[0].length;j++) {
+                if(grid[i][j] == '1') {
+                    for(int k=0;k<moveX.length;k++) {
+                        int x = i + moveX[k];
+                        int y = j + moveY[k];
+                        if(inArea(grid, x, y)) {
+                            if(grid[x][y] == '1') {
+                                u.union(i * grid[0].length + j, x * grid[0].length + y);
+                            }
+                            
+                        }
+                    }
+                }
+            }
+        }
+        return u.getSetNums();
+    }
+    private boolean inArea(char[][] grid, int r, int c) {
+        return 0 <= r && r < grid.length 
+                && 0 <= c && c < grid[0].length;
     }
     public static void main(String[] args) {
-        
+        IslandNumber landnumber = new IslandNumber();
+        int res = landnumber.numIslands(new char [][] {{'1','1','0','0'},{'0','0','1','1'}});
+        System.out.println("res is" + res);
     }
 }
---
+++ dfs/DFS通用框架代码.md
@@ -0,0 +1,69 @@
+### DFS的基本结构
+
+```java
+//二叉树下dfs遍历的基本结构
+void traverse(TreeNode root) {
+    // 判断 base case
+    if (root == null) {
+        return;
+    }
+    // 访问两个相邻结点：左子结点、右子结点
+    traverse(root.left);
+    traverse(root.right);
+}
+```
+
+```java
+//网格下dfs遍历的基本结构
+void dfs(int[][] grid, int r, int c) {
+    // 判断 base case
+    // 如果坐标 (r, c) 超出了网格范围，直接返回
+    if (!inArea(grid, r, c)) {
+        return;
+    }
+    // 访问上、下、左、右四个相邻结点
+    dfs(grid, r - 1, c);
+    dfs(grid, r + 1, c);
+    dfs(grid, r, c - 1);
+    dfs(grid, r, c + 1);
+}
+// 判断坐标 (r, c) 是否在网格中
+boolean inArea(int[][] grid, int r, int c) {
+    return 0 <= r && r < grid.length 
+        	&& 0 <= c && c < grid[0].length;
+}
+```
+
+#### 避免重复遍历
+
+![pic](https://pic.leetcode-cn.com/7fec64afe8ab72c5df17d6a41a9cc9ba3879f58beec54a8791cbf108b9fd0685.gif)
+
+如图，如果不做合适的标记，dfs很可能会“兜圈子”，所以需要进行标记。
+
+```java
+void dfs(int[][] grid, int r, int c) {
+    // 判断 base case
+    if (!inArea(grid, r, c)) {
+        return;
+    }
+    // 如果这个格子不是岛屿，直接返回
+    if (grid[r][c] != 1) {
+        return;
+    }
+    grid[r][c] = 2; // 将格子标记为「已遍历过」
+    
+    // 访问上、下、左、右四个相邻结点
+    dfs(grid, r - 1, c);
+    dfs(grid, r + 1, c);
+    dfs(grid, r, c - 1);
+    dfs(grid, r, c + 1);
+}
+
+// 判断坐标 (r, c) 是否在网格中
+boolean inArea(int[][] grid, int r, int c) {
+    return 0 <= r && r < grid.length 
+        	&& 0 <= c && c < grid[0].length;
+}
+
+```
+
