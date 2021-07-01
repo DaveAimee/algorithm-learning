cb9732286a85a5844affc90f8d3d41d565b6fe75
YangLiu <LotusRe@outlook.com>
2021-07-01 18:06:36 +0800

    dfs解题部分，不能完全通过，需要加上排序和并查集

--- src/main/java/chapter2_dsu/excersice/MaxMinPath.java
+++ src/main/java/chapter2_dsu/excersice/MaxMinPath.java
@@ -1,20 +1,24 @@
 package chapter2_dsu.excersice;
 
-import java.util.Queue;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class MaxMinPath {
-    private Boolean [][] isVisited = null;
+    private boolean [][] isVisited = null;
     // movX和moveY组成四个方向
     private int[] moveX = {-1,1,0,0};
     private int[] moveY = {0,0,-1,1};
     public int maximumMinimumPath(int[][] grid) {
-        this.isVisited = new Boolean[grid.length][grid[0].length];
-        
+        this.isVisited = new boolean [grid.length][grid[0].length];
+        Deque<Integer> st = new ArrayDeque<>();
+        int res = dfs(0, 0, grid, st);
+        return res;
     }
-    private void dfs(int i, int j, int[][] grid, Queue<Integer> path) {
+    private int dfs(int i, int j, int[][] grid, Deque<Integer> path) {
         if(!isVisited[i][j]) {
             if(i == grid.length -1 && j == grid[0].length -1) {
-                path.
+                path.add(grid[i][j]);
+                return path.stream().min(Integer::compareTo).get();
             }
             this.isVisited[i][j] = true;
             //查看四周没有被访问过的找到一个最大的方向进行检索
@@ -23,7 +27,7 @@ public class MaxMinPath {
             for(int k = 0;k < moveX.length;k++) {
                 int x = i + moveX[k];
                 int y = j + moveY[k];
-                if(inArea(grid, x, y)) {
+                if(inArea(grid, x, y) && !isVisited[x][y]) {
                     if(grid[x][y] > maxValue) {
                         maxValue = grid[x][y];
                         maxX = x;
@@ -32,9 +36,9 @@ public class MaxMinPath {
                 }
             }
             path.add(grid[i][j]);
-            dfs(maxX,maxY,grid,path);
+            return dfs(maxX,maxY,grid,path);
         }
-        
+        return -1;
     }
     private boolean inArea(int[][] grid, int r, int c) {
         return 0 <= r && r < grid.length
