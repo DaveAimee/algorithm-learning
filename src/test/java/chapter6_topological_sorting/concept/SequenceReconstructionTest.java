09f465cc5ba448e13c80a4625e3b4dd08dfa94b1
YangLiu <LotusRe@outlook.com>
2021-07-18 10:27:48 +0800

    提交拓扑排序（部分）

--- src/test/java/chapter6_topological_sorting/concept/SequenceReconstructionTest.java
+++ src/test/java/chapter6_topological_sorting/concept/SequenceReconstructionTest.java
@@ -6,6 +6,6 @@ public class SequenceReconstructionTest {
     @Test
     public void testSequenceReconstruction() {
         SequenceReconstruction s = new SequenceReconstruction();
-        s.sequenceReconstruction(new int[] {1,2,3},  List.of{{1,2},{1,3}});   
+        s.sequenceReconstruction(new int[] {1,2,3},  List.of(List.of(1,2),List.of(1,3)));   
     }
 }
