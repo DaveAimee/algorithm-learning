1eb9a478e7c12115e26a08d31151a985f4779205
YangLiu <LotusRe@outlook.com>
2021-07-19 17:55:21 +0800

    修改拓扑排序的代码格式

--- src/main/java/chapter6_topological_sorting/concept/SequenceReconstructionSolution.java
+++ src/main/java/chapter6_topological_sorting/concept/SequenceReconstructionSolution.java
@@ -1,14 +1,19 @@
 package chapter6_topological_sorting.concept;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
 //important
 //leetcode444(vip) : https://leetcode-cn.com/problems/sequence-reconstruction/
 public class SequenceReconstructionSolution {
     public boolean sequenceReconstruction(int[] org, List<List<Integer>> seqs) {
         int n = org.length;
-
         if (n == 0 || seqs.size() == 0) {
             return false;
         }
-
         // 考虑seqs里头元素为空列表的情况, 已经数字超过n或者<=0的情况
         Set<Integer> numSet = new HashSet<>();
         for (List<Integer> list : seqs) {
@@ -19,17 +24,13 @@ public class SequenceReconstructionSolution {
                 numSet.add(num);
             }
         }
-
         if (numSet.size() < n) {
             return false;
         }
-
-        ArrayList<Integer>[] adj = new ArrayList[n + 1];
-
+        ArrayList<Integer>[] adj = (ArrayList<Integer>[]) new ArrayList[n + 1];
         for (int i = 1; i <= n; i++) {
             adj[i] = new ArrayList<>();
         }
-
         // 构建邻接表
         for (int i = 0; i < seqs.size(); i++) {
             List<Integer> pair = seqs.get(i);
@@ -37,17 +38,14 @@ public class SequenceReconstructionSolution {
                 adj[pair.get(j)].add(pair.get(j + 1));
             }
         }
-
         // 计算每个节点的入度
         int[] inDegree = new int[n + 1];
-
         for (int i = 1; i <= n; i++) {
             for (int j = 0; j < adj[i].size(); j++) {
                 int w = adj[i].get(j);
                 inDegree[w]++;
             }
         }
-
         // 计算入度为0的节点，添加到队列中
         LinkedList<Integer> queue = new LinkedList<>();
         for (int i = 1; i <= n; i++) {
@@ -55,12 +53,10 @@ public class SequenceReconstructionSolution {
                 queue.addLast(i);
             }
         }
-
         // 入度为0的节点有多个，就会产生多种序列，或者没有入度为0的，不满足题目要求
         if (queue.size() != 1) {
             return false;
         }
-
         int index = 0;
         while (!queue.isEmpty()) {
             int num = queue.removeFirst();
@@ -68,7 +64,6 @@ public class SequenceReconstructionSolution {
                 return false;
             }
             index++;
-
             // 删除当前节点后，所有当前节点的下一个节点的入度为0的个数，超过1则说明序列不唯一
             int nextZeroInDegreeCount = 0;
             for (int j = 0; j < adj[num].size(); j++) {
@@ -79,12 +74,10 @@ public class SequenceReconstructionSolution {
                     if (nextZeroInDegreeCount > 1) {
                         return false;
                     }
-
                     queue.addLast(w);
                 }
             }
         }
-
         return index == n;
     }
 }
